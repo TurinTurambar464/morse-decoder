0ac355f9e70d37fd200a2cee6c8b1fdf9e167027
Dzianis Likhuta <belaruspartizan1654.1667@gmail.com>
2019-09-30 12:53:06 +0300

    feat: add task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,56 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   
+    let arr = Array.from(expr);
+    let subArr = []; 
+    let letterSize = 10; 
+
+    for(let i = 0; i < arr.length/letterSize; i++) {
+        subArr[i] = arr.slice(i*letterSize, i*letterSize + letterSize);
+    }
+
+    let interimArr = []; 
+    
+    for(let i = 0; i < subArr.length; i++) {  
+        let bitArr = [];  
+        for(let j = 0; j < subArr[i].length/2; j++) {                      
+            bitArr[j] = subArr[i].slice(j*2, j*2 + 2);
+        }
+        interimArr[i] = bitArr;
+    }             
+
+    for(let i = 0; i < interimArr.length; i++) {
+        for(let j = 0; j < interimArr[i].length; j++) {
+            interimArr[i][j] = interimArr[i][j].join('');
+            if(interimArr[i][j] == '10') {
+                interimArr[i][j] = '.'; 
+            } 
+            if(interimArr[i][j] == '11') {
+                interimArr[i][j] = '-';
+            }
+            if(interimArr[i][j] == '00') {
+                delete interimArr[i][j];
+            }
+        }
+    }
+
+    for(let i = 0; i < interimArr.length; i++) {
+        interimArr[i] = interimArr[i].join('');
+        if(interimArr[i] == '**********') {
+            interimArr[i] = ' ';
+        }
+    }
+   
+    for(let i = 0; i < interimArr.length; i++) {        
+        for (let [key, value] of Object.entries(MORSE_TABLE)) {
+            if(interimArr[i] == key) {
+                interimArr[i] = value;
+            }
+        }
+    }
+   
+    return interimArr.join('');    
 }
 
 module.exports = {
